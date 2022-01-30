ef9a57fc8f2a49eb6d7705576f66d08fe3ae3ef2
Selma Heric <selma@balkon.ba>
2022-01-30 22:44:27 +0100

    added counter component

--- src/containers/CounterPage/index.js
+++ src/containers/CounterPage/index.js
@@ -1,3 +1,20 @@
+import { useState } from 'react';
+import './styles.scss';
+
+function ButtonComponent({ onClick }) {
+  return (
+    <button className='button' onClick={onClick}>Increase</button>
+  )
+}
+
 export default function CounterPage() {
-  return <div>Counter page</div>
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className='counter'>
+      <div className='title'>Counter Page</div>
+      <div className='counterNumber'>Counter: {counter}</div>
+      <ButtonComponent onClick={() => setCounter(counter + 1)}/>
+    </div>
+  )
 }
